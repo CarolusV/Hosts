565dafe8b34933da971f88cf19e9ec26f1dba844
CarolusV <48219993+CarolusV@users.noreply.github.com>
2021-08-26 18:36:16 +0200

    Update redirector.js

--- AdGuard/Extensiones/AnimeFLV/redirector.js
+++ AdGuard/Extensiones/AnimeFLV/redirector.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name        Redirect m.animeflv.net to https://www3.animeflv.net/
 // @description On any web page it will check if the clicked links goes to userscripts.org. If so, the link will be rewritten to point to userscripts-mirror.org
-// @include     animeflv.net
+// @include     m.animeflv.net
 // @version     1.2
 // @grant       none
 // ==/UserScript==
 
 // This is a slightly brute force solution, but there is no other way to do it using only a userscript. A full-fledged addon may be created soon.
-// window.location.href =window.location.href.replace('m.animeflv.net','www3.animeflv.net');
+window.location.href =window.location.href.replace('m.animeflv.net','www3.animeflv.net');
 
 if(content.document.location == "https://m.animeflv.net"){
             window.location.replace("https://www3.animeflv.net/");
