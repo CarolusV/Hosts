bf31f8e1fe91efcda7dedd869a509e6c3e58ce13
CarolusV <48219993+CarolusV@users.noreply.github.com>
2021-09-04 18:54:18 +0200

    Update redirector.js

--- AdGuard/Extensiones/AnimeFLV/redirector.js
+++ AdGuard/Extensiones/AnimeFLV/redirector.js
@@ -6,24 +6,38 @@
 // @grant       none
 // ==/UserScript==
 
-
-//window.location.href =window.location.href.replace('https://m.animeflv.net','https://www3.animeflv.net');
-
-if (window.location.href.indexOf("https://m.animeflv.net") != -1){
-    window.location.href.replace('https://m.animeflv.net','https://www3.animeflv.net');
-}    
-
-if (window.location.href.indexOf("https://www3.animeflv.net") != -1){
+var doneTheStuff;
+function whatever() {
+  if (!doneTheStuff) {
+    doneTheStuff = true;
     
-} 
+	// end do the stuff
+	
+			window.location.href =window.location.href.replace('https://m.animeflv.net','https://www3.animeflv.net');
 
-if(content.document.location.indexOf("https://m.animeflv.net") != -1){
-            window.location.replace('https://m.animeflv.net','https://www3.animeflv.net');
+			if (window.location.href.indexOf("https://m.animeflv.net") != -1){
+				window.location.href.replace('https://m.animeflv.net','https://www3.animeflv.net');
+			}    
+
+			if (window.location.href.indexOf("https://www3.animeflv.net") != -1){
+				
+			} 
+
+			if(content.document.location.indexOf("https://m.animeflv.net") != -1){
+						window.location.replace('https://m.animeflv.net','https://www3.animeflv.net');
+			}
+
+			document.body.addEventListener('click', function(e){
+			    var targ = e.target || e.srcElement;
+			    if ( targ && targ.href && targ.href.match(/https?:\/\/m.animeflv.net/) ) {
+			        targ.href = targ.href.replace(/https?:\/\/m.animeflv.net/, 'https://www3.animeflv.net');
+			    }
+			}
+	
+	// do the stuff
+	
+  }
 }
 
-//document.body.addEventListener('click', function(e){
-//    var targ = e.target || e.srcElement;
-//    if ( targ && targ.href && targ.href.match(/https?:\/\/m.animeflv.net/) ) {
-//        targ.href = targ.href.replace(/https?:\/\/m.animeflv.net/, 'https://www3.animeflv.net');
-//    }
-});
+
+);
