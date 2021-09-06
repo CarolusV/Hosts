c7231c66f81aff0b5ca79f6d39c8e153788e8b07
CarolusV <48219993+CarolusV@users.noreply.github.com>
2021-09-06 20:23:52 +0200

    Update redirector.js

--- AdGuard/Extensiones/AnimeFLV/redirector.js
+++ AdGuard/Extensiones/AnimeFLV/redirector.js
@@ -2,7 +2,7 @@
 // @name        Redirect m.animeflv.net to https://www3.animeflv.net/
 // @description No m.animeflv.net, only www3.animeflv.net
 // @match       https://m.animeflv.net/*
-// @version     1.3
+// @version     1.4.2
 // @grant       none
 // @noframes
 // @run-at        document-start
@@ -20,27 +20,21 @@ function DOM_ContentReady () {
 function pageFullyLoaded () {
     //console.log ("==> Page is fully loaded, including images.", new Date() );
 	// end do the stuff
-	
 			window.location.href =window.location.href.replace('https://m.animeflv.net','https://www3.animeflv.net');
 
 			if (window.location.href.indexOf("https://m.animeflv.net") != -1){
 				window.location.href.replace('https://m.animeflv.net','https://www3.animeflv.net');
-			}    
-
-			if (window.location.href.indexOf("https://www3.animeflv.net") != -1){
-				
-			} 
+			}
 
 			if(content.document.location.indexOf("https://m.animeflv.net") != -1){
 						window.location.replace('https://m.animeflv.net','https://www3.animeflv.net');
 			}
 
-			document.body.addEventListener('click', function(e){
+			document.body.addEventListener('wheel', function(e){
 			    var targ = e.target || e.srcElement;
 			    if ( targ && targ.href && targ.href.match(/https?:\/\/m.animeflv.net/) ) {
 			        targ.href = targ.href.replace(/https?:\/\/m.animeflv.net/, 'https://www3.animeflv.net');
 			    }
-			}
-	
+			})
 	// do the stuff
-}
+;}
