081d27a725ceb724dbf0278d1598b037edb7589a
CarolusV <48219993+CarolusV@users.noreply.github.com>
2021-09-25 16:25:47 +0200

    Update animeflv_no_white_ver.js

--- AdGuard/Extensiones/AnimeFLV/animeflv_no_white_ver.js
+++ AdGuard/Extensiones/AnimeFLV/animeflv_no_white_ver.js
@@ -8,27 +8,23 @@
 // @resource      IMPORTED_CSS https://raw.githubusercontent.com/CarolusV/Hosts/master/AdGuard/Extensiones/AnimeFLV/custom_black.css
 // @include       animeflv.net
 // @run-at        document-start
-// @version       0.3.2
+// @version       0.3.3
 // ==/UserScript==
 
-(function()
 if (typeof GM_addStyle != "undefined") {
-	GM_addStyle(IMPORTED_CSS);
-} else if (typeof PRO_addStyle != "undefined") {
-	PRO_addStyle(IMPORTED_CSS);
-} else if (typeof addStyle != "undefined") {
-	addStyle(IMPORTED_CSS);
-} else {
+	GM_addStyle(IMPORTED_CSS);} 
+	else if (typeof PRO_addStyle != "undefined"){
+	PRO_addStyle(IMPORTED_CSS);} 
+	else if (typeof addStyle != "undefined"){
+	addStyle(IMPORTED_CSS);} 
+	else {
 	var node = document.createElement("style");
 	node.type = "text/css";
 	node.appendChild(document.createTextNode(IMPORTED_CSS));
 	var heads = document.getElementsByTagName("head");
-	if (heads.length > 0) {
-		heads[0].appendChild(node);
-	} else {
-		// no head yet, stick it whereever
-		document.documentElement.appendChild(node);
+		if (heads.length > 0){
+			heads[0].appendChild(node);} 
+			else {
+			// no head yet, stick it whereever
+			document.documentElement.appendChild(node);}
 	}
-}
-})();
-
