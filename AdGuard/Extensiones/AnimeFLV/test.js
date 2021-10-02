17323c608481cf14b7a954e2a3ea1e63a8189fcc
CarolusV <48219993+CarolusV@users.noreply.github.com>
2021-10-02 09:43:23 +0200

    Create test.js

---
+++ AdGuard/Extensiones/AnimeFLV/test.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name          AnimeFLV VerPage NoWhite
+// @description	  AnimeFLV VerPage NoWhite
+// @author        CarolusV
+// @homepage	  https://raw.githubusercontent.com/CarolusV/Hosts/master/AdGuard/Extensiones/AnimeFLV/animeflv_no_white_ver.js
+// @include       https://www3.animeflv.net
+// @include       http://www3.animeflv.net
+// @include       www3.animeflv.net
+// @include       animeflv.net
+// @run-at        document-start
+// @version       0.2
+// ==/UserScript==
+
+console.debug('start: add CSS');
+GM_xmlhttpRequest({
+  method: "GET",
+  url: "https://raw.githubusercontent.com/CarolusV/Hosts/master/AdGuard/Extensiones/AnimeFLV/custom_black.css",
+  onload: function(response) {
+    var css = response.responseText;
+    GM_addStyle(css);
+    console.debug('done: add ' + css);
+  }
+});
