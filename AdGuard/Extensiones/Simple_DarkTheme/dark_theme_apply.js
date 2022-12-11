cfb7f72962ba4df709bbd7cd9cb0cdf27fc4ade5
CarolusV <48219993+CarolusV@users.noreply.github.com>
2022-12-11 03:02:39 +0100

    Update dark_theme_apply.js

--- AdGuard/Extensiones/Simple_DarkTheme/dark_theme_apply.js
+++ AdGuard/Extensiones/Simple_DarkTheme/dark_theme_apply.js
@@ -5,7 +5,7 @@ var linkElement = document.createElement("link");
 linkElement.rel = "stylesheet";
 
 // Establecer el atributo "href" del elemento en la URL del archivo CSS que se desea aplicar
-linkElement.href = "https://example.com/styles.css";
+linkElement.href = "https://raw.githubusercontent.com/CarolusV/Hosts/master/AdGuard/Extensiones/Simple_DarkTheme/dark_theme.css";
 
 // Agregar el elemento <link> al documento
 document.head.appendChild(linkElement);
