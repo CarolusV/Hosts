a896d4c576cd944effa84cbd70486ee7409df766
CarolusV <48219993+CarolusV@users.noreply.github.com>
2023-01-07 15:41:13 +0100

    Update csgo_float.js

--- AdGuard/Extensiones/CSGO/csgo_float.js
+++ AdGuard/Extensiones/CSGO/csgo_float.js
@@ -8,47 +8,58 @@
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
-async function checkFloats() {
-  var items = document.getElementsByClassName("market_actionmenu_button");
-  var ids = [];
-  for (let i = 0; i < items.length; i++) ids.push(items[i].id);
-  var buttons = document.getElementsByClassName("market_listing_item_name_block");
-  var promises = [];
-  for (let i = 0; i < buttons.length - 1; i++) {
-    let node = buttons[i].getElementsByClassName("btn_green_white_innerfade btn_medium market_noncommodity_buyorder_button")[0];//
-    if (!node) continue;
-    promises.push(checkFloat(i, node));
-  }
-  await Promise.all(promises);
-  sortByFloat();
+function c() {
+    // Obtiene todos los elementos con la clase "market_actionmenu_button"
+    var items = document.getElementsByClassName("market_actionmenu_button");
+    var ids = [];
+    for (let i=0;i<items.length;i++)
+        ids.push(items[i].id);
+    // Obtiene todos los elementos con la clase "market_listing_item_name_block"
+    var buttons = document.getElementsByClassName("market_listing_item_name_block");
+    // Crea un array de objetos para almacenar la información de cada elemento
+    var elements = [];
+    for(let i = 0; i < buttons.length-1; i++) {
+        let node = document.createElement("a");
+        node.innerText = "check it!";
+        // Llamar a la función b() directamente en lugar de crear un evento onclick
+        b(i, node);
+        node.setAttribute("class", "btn_green_white_innerfade btn_medium market_noncommodity_buyorder_button");
+        node.style = "line-height: 20px;font-size: 10px;width:200px;text-align:center";
+        buttons[i].appendChild(node);
+        // Almacena la información del elemento en el array
+        elements.push({
+            float: 0, // Valor float del elemento (se actualizará más tarde)
+            node: buttons[i] // Nodo del elemento en el DOM
+        });
+    }
+    // Ordena el array de elementos de menor a mayor float
+    elements.sort(function(a, b) {
+        return a.float - b.float;
+    });
+    // Añade los elementos ordenados al DOM
+    for (let i = 0; i < elements.length; i++) {
+        document.getElementById("market_buyorder_info").childNodes[1].childNodes[1].appendChild(elements[i].node);
+    }
 }
-
-async function checkFloat(i, node) {
-  keys = Object.keys(g_rgAssets[730][2]);
-  link = g_rgAssets[730][2][keys[i]].market_actions[0].link;
-  link = link.replace("%assetid%", keys[i]);
-  return fetch("https://api.csgofloat.com/?url=" + link)
-    .then((response) => response.json())
-    .then((response) => {
-      node.innerText = response.iteminfo.floatvalue;
-      console.log(response.iteminfo.floatvalue);
+ 
+function b(i, node) {
+    keys = Object.keys(g_rgAssets[730][2]);
+    link = g_rgAssets[730][2][keys[i]].market_actions[0].link;
+    link = link.replace('%assetid%', keys[i]);
+    GM_xmlhttpRequest({
+        method: 'GET',
+        url: 'https://api.csgofloat.com/?url=' + link,
+        onload: response => {
+            response = JSON.parse(response.response);
+            node.innerText = response.iteminfo.floatvalue;
+            // Actualiza el valor float del elemento en el array
+            elements[i].float = response.iteminfo.floatvalue;
+            console.log(response.iteminfo.floatvalue);
+        }
     });
 }
 
-function sortByFloat() {
-  var rows = document.getElementsByClassName("market_listing_row");
-  var floats = [];
-  for (let i = 0; i < rows.length; i++) {
-    let floatValueElement = rows[i].getElementsByClassName("btn_green_white_innerfade btn_medium market_noncommodity_buyorder_button")[0];
-    if (floatValueElement) {
-      let floatValue = floatValueElement.innerText.trim();
-      floats.push({ row: rows[i], float: parseFloat(floatValue) });
-    }
-  }
-  floats.sort((a, b) => a.float - b.float);
-  for (let i = 0; i < floats.length; i++)
-    document.getElementById("market_commodity_forsale_listings").appendChild(floats[i].row);
-}
-
-window.addEventListener("load", checkFloats);
+document.addEventListener("DOMContentLoaded", function(event) {
+  c();
+});
 
