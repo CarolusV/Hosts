b175595c8734ffe72e24705af9e3ab9289d801c4
CarolusV <48219993+CarolusV@users.noreply.github.com>
2023-02-19 19:46:30 +0100

    Add files via upload

---
+++ AdGuard/Extensiones/NoDebuggerAllowed/noDebugger.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Disable debugger pausing on page
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Disables script from pausing debugger when debugging JavaScript code in the browser console
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+// Define a function that sets the "debugger" statement to "never pause"
+function disableDebuggerPause(event) {
+    event.preventDefault();
+}
+
+// Add an event listener to prevent the "debugger" statement from pausing the debugger
+window.addEventListener('beforeunload', () => {
+    const originalDebug = Object.getOwnPropertyDescriptor(window, 'debugger');
+    Object.defineProperty(window, 'debugger', {
+        get: () => {
+            return originalDebug.get.call(this);
+        },
+        set: (value) => {
+            if (value !== originalDebug.get.call(this)) {
+                originalDebug.set.call(this, value);
+                disableDebuggerPause(event);
+            }
+        }
+    });
+});
