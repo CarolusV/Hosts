bf4fe049ae3b38850648d69d15969c24ab45e646
CarolusV <48219993+CarolusV@users.noreply.github.com>
2024-11-03 02:10:06 +0100

    Create remove_things.js

---
+++ AdGuard/Extensiones/AnimeFLV/remove_things.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         Block Disqus and Comments
+// @version      1.0
+// @description  Elimina y bloquea Disqus, comentarios y elementos de reporte en un sitio web
+// @author       CarolusV
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Bloquear scripts de Disqus
+    const observer = new MutationObserver(() => {
+        const disqusScripts = document.querySelectorAll('script[src*="disqus.com"]');
+        disqusScripts.forEach(script => script.remove());
+    });
+
+    observer.observe(document, { childList: true, subtree: true });
+
+    // Eliminar elementos relacionados con comentarios, Disqus y reportes
+    function removeElementsBySelectors(selectors) {
+        selectors.forEach(selector => {
+            const elements = document.querySelectorAll(selector);
+            elements.forEach(element => element.remove());
+        });
+    }
+
+    // Ejecutar el proceso de eliminación de elementos en la carga de la página y cambios
+    function removeCommentsAndReports() {
+        removeElementsBySelectors([
+            '[id*="disqus"]',          // Contenedores relacionados con Disqus
+            '[class*="comment"]',      // Clases con comentarios
+            '[id*="comment"]',         // IDs con comentarios
+            '[class*="report"]',       // Clases con reportes
+            '[id*="report"]',          // IDs con reportes
+            '[data-disqus-identifier]' // Atributos que indican widgets de Disqus
+        ]);
+    }
+
+    // Ejecutar inicialmente
+    removeCommentsAndReports();
+
+    // Observador para detectar cambios dinámicos en la página
+    const config = { childList: true, subtree: true };
+    const mutationObserver = new MutationObserver(() => removeCommentsAndReports());
+    mutationObserver.observe(document.body, config);
+})();
